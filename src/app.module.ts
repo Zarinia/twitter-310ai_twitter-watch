20bf6d214edbe326015cce2b21e0944a7f37ac07
Zarinia <amirreza.zarinia@gmail.com>
2023-03-23 23:19:55 +0330

    update postman

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,6 @@ import { UsersModule } from '@users/users.module';
 import { PythonService } from '@python/python.service';
 import { WebModule } from '@/modules/web/web.module';
 import { DownloadService } from '@/modules/download/download.service';
-let MONGODB_USER = encodeURIComponent('tweeter');
-let MONGODB_PASS = encodeURIComponent('M3423DFC2345Rc5mc');
-// let mongodbUrl = `mongodb://${MONGODB_USER}:${MONGODB_PASS}@mongodb:27017/twitter`;
-// console.log( process.env.DB_URL);
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/twitter'),
